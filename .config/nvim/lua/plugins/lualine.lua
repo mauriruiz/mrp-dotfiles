f92a9c3530f6923cf837541d528be4ee87e09811
Mauri Ruiz <mauri.ruiz@webbeds.com>
2026-04-13 15:14:41 +0200

    full path file display

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,16 @@ return {
         options = {
           theme = 'monokai-pro',
         },
+        sections = {
+          lualine_c = {
+            { 'filename', path = 1 },
+          },
+        },
+        inactive_sections = {
+          lualine_c = {
+            { 'filename', path = 1 },
+          },
+        },
       })
     end
 }
